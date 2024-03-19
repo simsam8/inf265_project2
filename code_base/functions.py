Fixed MAP calculation for detection + refactoring + documentation

code_base/functions.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 from torchmetrics.detection import MeanAveragePrecision
 from datetime import datetime
 import time
-from object_detection import convert_to_dicts
+from .object_detection import MAP_preprocess
 
 
 def time_function(func):
@@ -200,10 +200,10 @@ def train(
                 total_predictions_train += n_preds
                 n_correct_train += batch_n_correct
             elif task == "detection":
-                outputs_reshaped = outputs.permute(0, 2, 3, 1).flatten(1, 2).view(1, 2, 3, 6)
-                labels_reshaped = labels.permute(0, 2, 3, 1).flatten(1, 2).view(1, 2, 3, 6)
-                preds = convert_to_dicts(outputs_reshaped)
-                targets = convert_to_dicts(labels_reshaped)
+                outputs_reshaped = outputs.permute(0, 2, 3, 1)
+                labels_reshaped = labels.permute(0, 2, 3, 1)
+                preds = MAP_preprocess(outputs_reshaped)
+                targets = MAP_preprocess(labels_reshaped)
                 train_metric.update(preds, targets)
 
         losses_train.append(loss_train / n_batch_train)
@@ -221,20 +221,23 @@ def train(
                 outputs = model(imgs)
                 loss = loss_fn(outputs, labels)
                 loss_val += loss.item()
-            if task == "localization":             
-                _, batch_n_correct, n_preds = compute_performance(
-                    outputs, labels, device
-                )
-                total_predictions_val += n_preds
-                n_correct_val += batch_n_correct
-            elif task == "detection":
-                outputs_reshaped = outputs.permute(0, 2, 3, 1).flatten(1, 2).view(1, 2, 3, 6)
-                labels_reshaped = labels.permute(0, 2, 3, 1).flatten(1, 2).view(1, 2, 3, 6)
-                preds = convert_to_dicts(outputs_reshaped)
-                targets = convert_to_dicts(labels_reshaped)
-                val_metric.update(preds, targets)
+                if task == "localization":             
+                    _, batch_n_correct, n_preds = compute_performance(
+                        outputs, labels, device
+                    )
+                    total_predictions_val += n_preds
+                    n_correct_val += batch_n_correct
+                elif task == "detection":
+                    outputs_reshaped = outputs.permute(0, 2, 3, 1)
+                    labels_reshaped = labels.permute(0, 2, 3, 1)
+                    preds = MAP_preprocess(outputs_reshaped)
+                    targets = MAP_preprocess(labels_reshaped)
+                    val_metric.update(preds, targets)
+
+            # Computes mean loss over batches
             losses_val.append(loss_val / n_batch_val)
-            if task == "localization":             
+
+            if task == "localization":          
                 performance_val.append(n_correct_val / total_predictions_val)
             else: 
                 performance_val.append(val_metric.compute()["map"])
@@ -244,10 +247,10 @@ def train(
             print(
                 f"{datetime.now().time()}\n"
                 f"Epoch: {epoch}\n"
-                f"train_loss:         {loss_train/n_batch_train:>10.3f}\n"
-                f"val_loss:           {loss_val/n_batch_val:>10.3f}\n"
-                f"train_performance:  {((n_correct_train / total_predictions_train)*100):>10.3f}%\n"
-                f"val_performance:    {((n_correct_val / total_predictions_val)*100):>10.3f}%\n"
+                f"train_loss:         {losses_train[-1]:>10.3f}\n"
+                f"val_loss:           {losses_val[-1]:>10.3f}\n"
+                f"train_performance:  {((performance_train[-1])*100):>10.3f}%\n"
+                f"val_performance:    {((performance_val[-1])*100):>10.3f}%\n"
             )
 
     return losses_train, losses_val, performance_train, performance_val
@@ -280,7 +283,10 @@ def train_models(
     """
     if task == "localization":
         loss_fn = localization_loss  # using our implemented loss function
-
+    elif task == "detection":
+        loss_fn = detection_loss
+    else: 
+        raise RuntimeError("set task to 'localization' or 'detection'")
     print("\tGlobal parameters:")
     print(f"Batch size: {batch_size}")
     print(f"Epochs: {n_epochs}")

code_base/object_detection.py
@@ -76,30 +76,43 @@ def _create_grid_boxes(
     output_box = output_box.permute(2, 0, 1)  # CxHxW
     return output_box
 
-def convert_to_dicts(inputs: torch.Tensor, grid_height, grid_width, threshold: int=0.5):
-    """Prepares data for torchmetrics.MAP calculation. 
-    Box format: cx cy w h"""
-    preds = list()
+def MAP_preprocess(input_tensor: torch.Tensor, threshold: int=0.5):
+    """Preprocesses the label or prediction tensor for torchmetrics.MAP calculation. 
+    Box format: cx cy w h
+    Return: A dictionary of Tensor values as specified in torchmetrics docs. """
+    
+    dict_for_MAP =  []
+    # New dimensions: (batch size, grid height, grid width, channels)
+    input_tensor = input_tensor.permute(0, 2, 3, 1)
 
-    for batch in range(input.shape[0]):
-        image_preds = {
+    # Loop over all images in the batch
+    for batch in range(input_tensor.shape[0]):
+        # Store prediction or target values in a dict
+        img_detect = {
         "boxes": [],
         "scores": [],
         "labels": []
         }
-        for h in range(grid_height):
-            for w in range(grid_width):
-                cell = inputs[batch, h, w]
+        # Extracts data from tensor
+        for h in range(input_tensor.shape[1]):
+            for w in range(input_tensor.shape[2]):
+                cell = input_tensor[batch, h, w]
+                # P_c: detection confidence
                 P_c = cell[0]
-                if P_c >= inputs:
-                    # Assuming box coordinates are normalized [0, 1] relative to image dimensions
-                    box = cell[1:5]  # x_min, y_min, x_max, y_max
-                    label = cell[5].long()  # Class label
-                    image_preds["boxes"].append(torch.tensor(box.tolist()))
-                    image_preds["scores"].append(torch.tensor(P_c.item()))
-                    image_preds["labels"].append(torch.tensor(label.item()))
+                if P_c >= threshold:
+                    box = cell[1:5] 
+                    label = cell[5].long()
+                    img_detect["boxes"].append(box.tolist())
+                    img_detect["scores"].append(P_c.item())
+                    img_detect["labels"].append(label.item())
 
-        preds.append(image_preds)
+        # Converts all dict values to Tensors and stores image detection data in dict
+        img_detect["boxes"] = torch.tensor(img_detect["boxes"])
+        img_detect["scores"] = torch.tensor(img_detect["scores"])
+        img_detect["labels"] = torch.tensor(img_detect["labels"])
+        dict_for_MAP.append(img_detect)
+
+    return dict_for_MAP
 
 
 def get_converted_data(