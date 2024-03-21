4f4a187fe8a9ecb9185e914083a9146d39aecdce
simsam8 <simvedaa@gmail.com>
2024-03-21 14:37:20 +0100

    add new architectures + remove LeNetVariant

--- code_base/models.py
+++ code_base/models.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class TestNet(nn.Module):
+class LocalNet1(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.conv1 = nn.Conv2d(1, 3, 3)
@@ -14,7 +14,7 @@ class TestNet(nn.Module):
         self.fc3 = nn.Linear(60, 15)
 
     def forward(self, x):
-        # Input images of size 1x48x60
+        # Input size of 1x48x60
         x = F.relu(self.conv1(x))  # 3x46x58
         x = self.max_pool(x)  # 3x23x29
         x = F.relu(self.conv2(x))  # 8x21x27
@@ -25,45 +25,39 @@ class TestNet(nn.Module):
         x = self.fc3(x)
         return x
 
-    def __str__(self) -> str:
-        return "TestNet"
 
-
-class LeNetVariant(nn.Module):
-    """
-    Architecture based on LeNet, only input and output dimensions are changed
-    """
-
+class LocalNet2(nn.Module):
     def __init__(self) -> None:
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5, padding=2)
-        self.avg_pool = nn.AvgPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5, padding=0)
-        self.fc1 = nn.Linear(16 * 10 * 13, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 15)
+        self.conv1 = nn.Conv2d(1, 6, 3, padding=1)
+        self.max_pool2 = nn.MaxPool2d(2, 2)
+        self.max_pool3 = nn.MaxPool2d(3, 3)
+        self.conv2 = nn.Conv2d(6, 12, 3, padding=1)
+        self.conv3 = nn.Conv2d(12, 24, 3, padding=1)
+        self.conv4 = nn.Conv2d(24, 48, 3, padding=1)
+        self.fc1 = nn.Linear(48 * 4 * 5, 960)
+        self.fc2 = nn.Linear(960, 320)
+        self.fc3 = nn.Linear(320, 80)
+        self.fc4 = nn.Linear(80, 15)
 
     def forward(self, x):
         # Input of size 1x48x60
-        x = F.sigmoid(self.conv1(x))  # 6x48x60
-        x = self.avg_pool(x)  # 6x24x30
-        x = F.sigmoid(self.conv2(x))  # 16x20x26
-        x = self.avg_pool(x)  # 16x10x13
+        x = F.relu(self.conv1(x))  # 6x48x60
+        x = self.max_pool2(x)  # 6x24x30
+        x = F.relu(self.conv2(x))  # 12x24x30
+        x = self.max_pool2(x)  # 12x12x15
+        x = F.relu(self.conv3(x))  # 24x12x15
+        x = self.max_pool3(x)  # 24x4x5
+        x = F.relu(self.conv4(x))  # 48x4x5
         x = torch.flatten(x, 1)
-        x = F.sigmoid(self.fc1(x))
-        x = F.sigmoid(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
-    def __str__(self) -> str:
-        return "LeNetVariant"
 
-
-class ObjectDetect_2x3(nn.Module):
-    """
-    Architecture with output of 2x3 grid
-    """
-
+class DetectNet1_2x3(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.conv1 = nn.Conv2d(1, 2, 3, padding=1)
@@ -74,7 +68,7 @@ class ObjectDetect_2x3(nn.Module):
         self.conv4 = nn.Conv2d(6, 6, 3, padding=0)
 
     def forward(self, x):
-        # Input 1x48x60
+        # Input of size 1x48x60
         x = F.relu(self.conv1(x))  # 2x48x60
         x = self.max_pool(x)  # 2x24x30
         x = F.relu(self.conv2(x))  # 4x24x30
@@ -82,4 +76,36 @@ class ObjectDetect_2x3(nn.Module):
         x = F.relu(self.conv3(x))  # 6x12x15
         x = self.max_pool_by_3(x)  # 6x4x5
         x = self.conv4(x)  # 6x2x3
+        return x
+
+
+class DetectNet2_2x3(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.max_pool2 = nn.MaxPool2d(2, 2)
+        self.max_pool3 = nn.MaxPool2d(3, 3)
+        self.conv1 = nn.Conv2d(1, 2, 3, padding=1)
+        self.conv2 = nn.Conv2d(2, 4, 3, padding=1)
+        self.conv3 = nn.Conv2d(4, 8, 3, padding=1)
+        self.conv4 = nn.Conv2d(8, 16, 3, padding=1)
+        self.conv5 = nn.Conv2d(16, 32, 3, padding=1)
+        self.conv6 = nn.Conv2d(32, 32, 3, padding=1)
+        self.conv7 = nn.Conv2d(32, 16, 3, padding=1)
+        self.conv8 = nn.Conv2d(16, 8, 3, padding=1)
+        self.conv_out = nn.Conv2d(8, 6, 3, padding=0)
+
+    def forward(self, x):
+        # Input of size 1x48x60
+        x = F.relu(self.conv1(x))  # 2x48x60
+        x = F.relu(self.conv2(x))  # 4x48x60
+        x = self.max_pool2(x)  # 4x24x30
+        x = F.relu(self.conv3(x))  # 8x24x30
+        x = F.relu(self.conv4(x))  # 16x24x30
+        x = self.max_pool2(x)  # 16x12x15
+        x = F.relu(self.conv5(x))  # 32x12x15
+        x = F.relu(self.conv6(x))  # 32x12x15
+        x = self.max_pool3(x)  # 32x4x5
+        x = F.relu(self.conv7(x))  # 16x4x5
+        x = F.relu(self.conv8(x))  # 8x4x5
+        x = self.conv_out(x)  # 6x2x3
         return x
