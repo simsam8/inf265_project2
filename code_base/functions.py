70f302b9bb95849fbb22cecc4804a1cf1803f877
simsam8 <simvedaa@gmail.com>
2024-03-21 14:34:54 +0100

    remove wrapper function

--- code_base/functions.py
+++ code_base/functions.py
@@ -5,19 +5,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchmetrics.detection import MeanAveragePrecision
 from datetime import datetime
-import time
 from .object_detection import MAP_preprocess
-
-
-def time_function(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"{func.__name__} took {end_time-start_time} seconds to complete.")
-        return result
-
-    return wrapper
 
 
 def localization_loss(y_pred: torch.Tensor, y_true: torch.Tensor) -> torch.Tensor:
